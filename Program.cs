2fafa0610d4bec2d6bb37af467d6b3e90b3c88c1
Shaxboz <85881220+ShakhbozDeveloper0496@users.noreply.github.com>
2024-02-24 13:01:11 +0500

    aylanani yuzini va uzunligini hisoblash

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Doira yuzi va aylana uzunligini hisoblash
+
+Console.WriteLine("Radiusni kiriting: ");
+double radFirst = Convert.ToDouble(Console.ReadLine());
+decimal rad = (decimal)radFirst;
+double pi = Math.PI;
+decimal piBig = (decimal)pi;
+decimal aylanaS = piBig * rad * rad;
+decimal aylanaL = 2 * piBig * rad;
+Console.WriteLine($"S= {aylanaS}");
+Console.WriteLine($"L= {aylanaL}");
+
+
+
+
+
+
+
